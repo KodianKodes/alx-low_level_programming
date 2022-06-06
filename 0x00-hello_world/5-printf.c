c6c1c254e63373263cb15dff62bb615220ba2f43
kodiankodes <arthuremmanuel270@gmail.com>
2022-06-06 03:38:21 +0100

    This is too easy with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point of every c programm
+ *
+ * Return 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the out come is a piece of art,\n");
+	return (0);
+}
