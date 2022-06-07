4907a573ff61684fb0511d9567574c6659932c8c
kodiankodes <arthuremmanuel270@gmail.com>
2022-06-07 19:30:12 +0100

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always success (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the out outcome is a pieace of art,\n");
+	return (0);
+}
