49196a6df8e75b06fa6aa393066ca39d8015837b
kodiankodes <arthuremmanuel270@gmail.com>
2022-06-07 19:37:23 +0100

    fixing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the out outcome is a pieace of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
