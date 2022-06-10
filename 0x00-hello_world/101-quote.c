024b4986f64ad9356a02ddfa27811f38fc6fd2ea
kodiankodes <arthuremmanuel270@gmail.com>
2022-06-10 12:14:07 +0400

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" -
+			Dora Korpar, 2015-10-19\n");
+	return (1);
+}
